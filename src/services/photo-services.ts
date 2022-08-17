eea2819d64d267ccb2bb84e2a6b57983a9b0c804
Carla Pico <carlaspico@gmail.com>
2022-08-17 12:00:55 -0400

    enhanced to be able to get post and patch

--- src/services/photo-services.ts
+++ src/services/photo-services.ts
@@ -1,10 +1,11 @@
+import { ObjectId } from "mongodb";
 import { db } from "../db/db";
 import { Photo } from "../models/photo";
 
 interface PhotoServices {
     updateLikes (id:string, inc:number) : Promise<Photo>
     createPhoto(photo:Photo): Promise<string>
-    createComment(id:string, comment:string): Promise <Photo>
+    // createComment(id:string, comment:string): Promise <Photo>
     getAllPhotos(): Promise<Photo[]>
 }
 
@@ -13,5 +14,34 @@ const photoCollection= db.collection<Photo>("photos")
 export const getAllPhotos= async (): Promise<Photo[]> => {
     const photos= await photoCollection.find().toArray()
 
-    return photos;
+    return photos
+}
+
+export const createPhoto = async (photo: Photo): Promise<string> => {
+    try {
+        const res= await photoCollection.insertOne(photo)
+    
+        return res.insertedId.toString()
+
+    } catch (error) {
+        return "Something went wrong"
+
+    }
+}
+
+export const updateLikes = async (id: string, inc:number = 1): Promise<Photo> => {
+    const res= await photoCollection.findOneAndUpdate( 
+        { _id: new ObjectId(id) }, 
+        { $inc: { likes: inc} } 
+    )
+    const updatedPhoto = res.value as Photo
+    return updatedPhoto
+
+}
+
+// export const createComment = async (id:string, comment:string): Promise<Photo> => {
+// }
+
+export const photoServices: PhotoServices = { getAllPhotos, createPhoto, updateLikes 
+    // createComment 
 }
